c502cd06f6c52f0495006e2f78aab11f8e410a71
Emelly Beatriz <emellybmuniz@gmail.com>
2025-09-24 19:50:11 -0300

    Add files via upload

---
+++ web/html_css/exercicios-adicionais/ex013/index.js
@@ -0,0 +1,32 @@
+// String
+let nome = "João"; // Nome: João
+
+// Number
+let idade = 25;
+
+// Boolean
+let maiorDeIdade = true; // Maior de idade: true
+
+// Symbol
+let simbolo = Symbol('descricao'); // Símbolo: Symbol(descricao)
+
+// 5. Undefined
+let variavelIndefinida; // Valor indefinido: undefined
+
+// 6. Null
+let valorNulo = null; // valor nulo: null
+
+
+const exemploArray = ['teste', 1, true, null] // [ 'teste', 1, true, null ]
+
+const exemploObjeto = {
+    valor: null // { valor: null }
+
+}
+
+function exemploFuncao ()  {
+    return null
+} // null
+
+
+
